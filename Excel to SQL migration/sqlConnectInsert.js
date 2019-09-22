7407710d1b0dbff8a6fef20a3383aaddf3cb09c1
justindelisi <47611223+justindelisi@users.noreply.github.com>
2019-09-22 15:42:05 -0400

    Create sqlConnectInsert.js

---
+++ Excel to SQL migration/sqlConnectInsert.js
@@ -0,0 +1,42 @@
+const sqlite3 = require('sqlite3').verbose();
+
+class insertIntoDatabase {
+    //constructor connects to database with file path given
+    constructor(dbFilePath){
+        this.db = new sqlite3.Database(dbFilePath, (err)=> {
+            if (err){
+                return console.error(err.message);
+            }
+            console.log('Connected to database');
+        })
+    }
+
+    //insert into PG1_Company table
+    pg1_CompanyInsert(name, addr1, addr2, city, state, zip, congressionalDistrict) {
+        this.db.run(`INSERT INTO pg1_company (name, addr1, addr2, city,
+             state, zip, congressionalDistrict) VALUES(?,?,?,?,?,?,?)`,
+        [name, addr1, addr2, city, state, zip, congressionalDistrict])
+    }
+
+    //insert into PG1_Media table
+    pg1_MediaInsert(filePath, fileType, description, medLength, source, compId) {
+        this.db.run(`INSERT INTO pg1_media (filePath, fileType, description, 
+            medLength, source, compId) VALUES(?,?,?,?,?,?)`,
+        [filePath, fileType, description, medLength, source, compId])
+    }
+
+    //insert into PG1_Award table
+    pg1_AwardInsert(piid, compId, currentTotal, potentialTotal, parentAwardAgency, awardingAgency,
+        fundingOffice, fiscalYear) {
+        this.db.run(`INSERT INTO pg1_award (piid, compId, currentTotal, potentialTotal, 
+            parentAwardAgency, awardingAgency, awardingOffice, 
+            fundingOffice, fiscalYear) VALUES(?,?,?,?,?,?,?,?,?)`,
+        [piid, compid, currentTotal, potentialTotal, parentAwardAgency, 
+            awardingOffice, fundingOffice, fiscalYear  ])
+    }
+
+    closeDb(){
+        this.db.close();
+    }
+
+}
