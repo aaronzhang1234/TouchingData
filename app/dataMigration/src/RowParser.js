bfbae4d164d5ebcd791f7377b39bc63a88038af9
aaronzhang1234 <aaronzhang.xyz@gmail.com>
2019-10-04 17:49:23 -0400

    used ownership types

--- app/dataMigration/src/RowParser.js
+++ app/dataMigration/src/RowParser.js
@@ -6,76 +6,37 @@ var dao = new Dao(sqlDatabaseName);
 
 const types = ["alaskan_native_owned_corporation_or_firm", "american_indian_owned_business", "indian_tribe_federally_recognized", "native_hawaiian_owned_business", "tribally_owned_business", "veteran_owned_business", "service_disabled_veteran_owned_business", "woman_owned_business", "women_owned_small_business", "economically_disadvantaged_women_owned_small_business", "joint_venture_women_owned_small_business", "joint_venture_economic_disadvantaged_women_owned_small_business", "asian_pacific_american_owned_business", "black_american_owned_business", "hispanic_american_owned_business", "native_american_owned_business", "other_minority_owned_business"];
 
-let State = require("../../models/State.js")
-let District = require("../../models/District.js");
 let PlaceOfPerformance = require("../../models/PlaceOfPerformance");
 let Ownership = require("../../models/RecipientOwnershipType.js");
-var Type = require("../../models/OwnershipType.js");
-
-
 let ParentAwardAgency = require("../../models/ParentAwardAgency");
+let RecipientParent = require("../../models/RecipientParent.js");
 let AwardingAgency = require("../../models/AwardingAgency");
 let Recipient = require("../../models/Recipient.js");
-let Award = require("../../models/Award.js");
-let RecipientParent = require("../../models/RecipientParent.js");
+var Type = require("../../models/OwnershipType.js");
 let Office = require("../../models/Office.js");
+let Award = require("../../models/Award.js");
+
 class RowParser{
 	constructor(row){
 		this.row = row
-	}
+    }
 
-	// states should be handled by the InitValidationTables script
-	//Inserting recipients info vs inserting place of performance
-	//insertState(is_recipient){
-	//	let state_code, state_name = "";
-	//      if(is_recipient){
-	//	    state_code = this.row.getCell(13).value;
-	//          state_name = this.row.getCell(14).value;
-	//      }else{
-	//	    state_code = this.row.getCell(19).value;
-	//          state_name = this.row.getCell(20).value;
-	//      }
-	//	let state = new State(state_code, state_name);
-	//	dao.insertState(state);
-	//  }
-	//  districts should be handled by  initvalidationsTables Script
-	//   insertDistrict(is_recipient){
-	//      let district_num = 0 
-	//      let state_code = "";
-	//      if(is_recipient){
-	//          district_num = this.row.getCell(16).value;
-	//          state_code = this.row.getCell(13).value;
-	//      }else{
-	//          district_num = this.row.getCell(22).value;
-  //          state_code = this.row.getCell(19).value;
-  //      }
-  //      let district = new District(district_num, state_code);
-  //      dao.insertDistrict(district);
-  //  }
     insertPlaceOfPerformance(){
         let pop_city = this.row.getCell(17).value;
         let pop_county = this.row.getCell(18).value;
         let pop_statecode = this.row.getCell(19).value;
+        let pop_zip = this.convertToNoDecimal(this.row.getCell(21).value);
+        let pop_district = this.convertToNoDecimal(this.row.getCell(22).value);
 
-        let pop_zip = this.row.getCell(21).value;
-        if(pop_zip != null){
-            pop_zip = this.convertToInt(pop_zip); 
-        }
-
-        let pop_district = this.row.getCell(22).value;
-        if(pop_district != null){
-            pop_district = this.convertToInt(pop_district); 
-        }
-    
-        let placeofperformance = new PlaceOfPerformance("", pop_city, pop_county, pop_statecode, pop_zip, pop_district);
-        dao.insertPlace(placeofperformance);
+        let place_of_performance = new PlaceOfPerformance("", pop_city, pop_county, pop_statecode, pop_zip, pop_district);
+        dao.insertPlace(place_of_performance);
     }
     //complete enough
     insertRecipientParent(){
         let parent_name = this.row.getCell(7).value;
         
-        let recipientparent = new RecipientParent("", parent_name);
-        dao.insertRecParent(recipientparent);
+        let recipient_parent = new RecipientParent("", parent_name);
+        dao.insertRecParent(recipient_parent);
     }
     insertRecipient(){
         let name = this.row.getCell(6).value; 
@@ -83,25 +44,26 @@ class RowParser{
         let addr2 = this.row.getCell(11).value; 
         let city = this.row.getCell(12).value; 
         let state_code = this.row.getCell(13).value; 
-        let zip = this.convertToInt(this.row.getCell(15).value); 
-        let district = this.convertToInt(this.row.getCell(16).value);
+        let zip = this.convertToNoDecimal(this.row.getCell(15).value); 
+        let district = this.convertToNoDecimal(this.row.getCell(16).value);
 
+        //Getting the object by name, if object is null then set id to null else set id to object.id
         let parent_name = this.row.getCell(7).value
         let parent = dao.selectRecipientParentbyName(parent_name);
-        let parentid = "";
+        let parent_id = "";
         if(parent != null){
-             parentid = parent.id; 
+             parent_id = parent.id; 
         }
 
-        let pop_city = this.convertToInt(this.row.getCell(17).value);
-        let pop_zip = this.convertToInt(this.row.getCell(21).value);
+        let pop_city = this.convertToNoDecimal(this.row.getCell(17).value);
+        let pop_zip = this.convertToNoDecimal(this.row.getCell(21).value);
         let PoP = dao.selectPlacePerformance(pop_city, pop_zip);
-        let PoPid = "";
+        let PoP_id = "";
         if(PoP != null){
-            PoPid = PoP.id; 
+            PoP_id = PoP.id; 
         }
 
-        let recipient = new Recipient("", name, addr, addr2, city, state_code, zip, parentid, district, "", PoPid );
+        let recipient = new Recipient("", name, addr, addr2, city, state_code, zip, parent_id, district, "", PoP_id );
         dao.insertRecipient(recipient);
     }
     insertRecipientOwnershipTypes(){    
@@ -138,19 +100,19 @@ class RowParser{
     insertParentAwardAgency(){
         let parent_award_agency_name = this.row.getCell(2).value;
 
-        let parentawardagency = new ParentAwardAgency("", parent_award_agency_name);
-        dao.insertParentAward(parentawardagency);
+        let parent_award_agency = new ParentAwardAgency("", parent_award_agency_name);
+        dao.insertParentAward(parent_award_agency);
     }
     insertAwardingAgency(){
         let awarding_agency_name = this.row.getCell(3).value;
         let parent_award_agency_name = this.row.getCell(2).value;
         let paa = dao.selectParentAwardingAgency(parent_award_agency_name);
-        let paaid = "";
+        let paa_id = "";
         if(paa !=null){
-            paaid = paa.id;
+            paa_id = paa.id;
         }
-        let awardingagency = new AwardingAgency("", awarding_agency_name, paaid);
-        dao.insertAwardingAgency(awardingagency);
+        let awarding_agency = new AwardingAgency("", awarding_agency_name, paa_id);
+        dao.insertAwardingAgency(awarding_agency);
     }
     insertOffices(){
         let awarding_office_name = this.row.getCell(8).value;
@@ -164,6 +126,7 @@ class RowParser{
     }
     insertAward(){
         let award_piid = this.row.getCell(1).value;
+        console.log(award_piid);
         let fiscal_year = this.row.getCell(42).value; 
 
         let recipient_name = this.row.getCell(6).value;
@@ -194,26 +157,34 @@ class RowParser{
         let funding_office = dao.selectOffice(funding_office_name);
         let funding_office_id = "";
         if(funding_office != null){
-            funding_office_id = this.convertToInt(funding_office.id);
+            funding_office_id = this.convertToNoDecimal(funding_office.id);
         }
 
         let award = new Award(award_piid, fiscal_year, recipient_id, current_total, potential_total, awarding_agency_id, awarding_office_id, funding_office_id);
         dao.insertAward(award);
     }
 
-	insertOwnerships(recipientId){
+	insertOwnerships(){
+        let recipient_name = this.row.getCell(6).value;
+        let recipient = dao.selectRecipientByName(recipient_name);
+        let recipient_id = "";
+        if(recipient != null){
+            recipient_id = recipient.id;
+        }
+        let thisthat = this;
 		types.forEach(function (type, i){
 			//the first ownership type is at column 23 in the worksheet
-			if (row.getCell(i + 23).value === "t"){
-				ownership = new Ownership(type, recipientId, "");
-				insertRecOwnership(ownership);
+			if (thisthat.row.getCell(i + 23).value === "t"){
+				let ownership = new Ownership(type, recipient_id, "");
+				dao.insertRecOwnership(ownership);
 			}
 		});
-	}
-    convertToInt(num){
-        let numString = String(num);
-        let numArray = numString.split(".");
-        return numArray[0];
+    }
+    //Do not insert as decimals instead insert as ints
+    convertToNoDecimal(num){
+        let num_string = String(num);
+        let num_array = num_string.split(".");
+        return num_array[0];
     }
 }
 
