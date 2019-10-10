9deca7c25c5b14623d282c411e5a19094bb046ea
aaronzhang1234 <aaronzhang.xyz@gmail.com>
2019-10-10 18:31:41 -0400

    added bing webscraping to main function and dump file with websites

--- app/web-scraping/src/main.js
+++ app/web-scraping/src/main.js
@@ -8,7 +8,7 @@
 const webscraperjs = require("./webscraper.js");
 const DAO = require("../../DAO.js");
 let sqlDatabaseName = "data/POLITICS_OF_THE_GRID.db";
-
+let Website = require("../../models/Website.js");
 
 var webscraper = new webscraperjs();
 //webscraper.getSiteFromName("Federal Express Corporation");
@@ -19,14 +19,25 @@ let dao = new DAO(sqlDatabaseName);
 let recipients = dao.selectAllRecipients();
 //console.log(recipients);
 
-for(let i =0; i<10; i++){
-    let recipient = recipients[i];
-    webscraper.getSiteFromName(recipient.name).then(function(url){
-        dao.insertWebsite(url);
-        console.log(url);
-    });
+let time = 1000;
+for(let i =0; i<recipients.length; i++){
+    time = time + 3000;
+    let recipient = recipients[i];    
+    setTimeout(function(){
+        webscraper.getSiteFromName(recipient.name).then(function(url){
+            let website = new Website("", url);
+            dao.insertWebsite(website);
+            console.log(url);        
+            website = dao.selectWebsite(url);
+            console.log(website.id);
+            let num_string = String(website.id);
+            let num_array = num_string.split(".");
+            let website_id =  num_array[0];
+            dao.updateRecipientWebsite(recipient.id, website_id);
+        });
+    }, time);
 }
 
 
 
-//webscraper.getSite("https://www.varidesk.com/", "https://www.varidesk.com/",[], stopTime);
+//webscraper.getSite("https://adsinc.com/", "https://adsinc.com/",[], stopTime);
