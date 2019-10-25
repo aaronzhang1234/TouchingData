e74502e4a5e3a3c0022ac0581abe34f8e37c7b47
aaronzhang1234 <aaronzhang.xyz@gmail.com>
2019-10-25 01:48:19 -0400

    ANGULAR SERVER IS NOW UP AND RUNNING

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const http = require("http");
+const path = require("path");
+const WS_Controller = require("./app/web-scraping/src/webscraper_controller")
 var app = express();
 
-app.get("/", function(req, res){
-    res.send("Hello World!");
+app.use(express.static(__dirname + '/dist/dashboard'));
+
+app.get("/*", function(req, res){
+    res.sendfile(path.join(__dirname));
 });
 
-app.listen(3000, function(){
-    console.log("fucking works");
-})
+app.get("/whelp", function(req, res){
+    let webscraper = new WS_Controller();
+    webscraper.webscrapeAllSites();
+});
+
+const server = http.createServer(app);
+
+server.listen(3000, ()=>console.log("fug"));
