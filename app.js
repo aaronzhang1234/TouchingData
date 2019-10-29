4127319e5f1db0e63a2653a3f0406c3a619bef5d
Hadi Haidar <hadihaidar@Hadis-MacBook-Pro-2.local>
2019-10-28 20:46:02 -0400

    Hadi integration

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
-const WS_Controller = require("./app/web-scraping/src/webscraper_controller")
-const DataMigrator = require("./app/dataMigration/src/DataMigrator.js")
-const bodyparser = require('body-parser');
+const WS_Controller = require("./app/web-scraping/src/webscraper_controller");
+const DataMigrator = require("./app/dataMigration/src/DataMigrator.js");
+const DbBuilder = require("./app/dataMigration/srs/DbBuilder.js");
+const bodyparser = require("body-parser");
 
 var app = express();
 
-app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-app.use(express.static(__dirname + '/dist/dashboard'));
+app.use(express.static(__dirname + "/dist/dashboard"));
 
-app.get("/*", function(req, res){
-    res.sendfile(path.join(__dirname));
+app.get("/*", function(req, res) {
+  res.sendfile(path.join(__dirname));
 });
 
-app.get("/whelp", function(req, res){
-    let webscraper = new WS_Controller();
-    webscraper.webscrapeAllSites();
+app.get("/whelp", function(req, res) {
+  let webscraper = new WS_Controller();
+  webscraper.webscrapeAllSites();
 });
 
-app.post("/import", function(req,res){
-	console.log(req.body)
-	let dataMigrator = new DataMigrator();
-	dataMigrator.migrateData();
-	res.send({status : "success"});
-})
+app.post("/buildDb", function(req, res) {
+  console.log(req.body);
+  let dbBuilder = new DbBuilder();
+  dbBuilder.buildDb();
+  res.send({ status: "Database Built!" });
+});
+app.post("/import", function(req, res) {
+  console.log(req.body);
+  let dataMigrator = new DataMigrator();
+  dataMigrator.migrateData();
+  res.send({ status: "success" });
+});
 
 const server = http.createServer(app);
 
-server.listen(3000, ()=>console.log("fug"));
+server.listen(3000, () => console.log("fug"));
--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,33 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { Component, OnInit } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.scss"]
 })
 export class DashboardComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
-  
- fileName : string = "ChooseFile";
+  fileName: string = "ChooseFile";
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  buildDb() {
+    const headers = new HttpHeaders().set("Content-Type", "application/json");
+
+    this.http
+      .post("/buildDb", { status: "Go" }, { headers: headers })
+      .subscribe(data => {
+        console.log(data);
+      });
   }
 
   import() {
     let bar = document.getElementById("progressbar");
     bar.setAttribute("style", "display:inline-block;");
 
-		const headers = new HttpHeaders()
-			.set('Content-Type', 'application/json')
+    const headers = new HttpHeaders().set("Content-Type", "application/json");
 
-		console.log("you touched me")
-		this.http.post("/import",{fileName: this.fileName} , {headers : headers})
-			.subscribe(data => {
-				console.log(data);
-			});
-	}
-
+    console.log("you touched me");
+    this.http
+      .post("/import", { fileName: this.fileName }, { headers: headers })
+      .subscribe(data => {
+        console.log(data);
+      });
+  }
 }
