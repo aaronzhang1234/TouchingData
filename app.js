53a7127df8cbbc25c2c89d5a50d7b6df756bbfb6
Nate <nawtrobar@gmail.com>
2019-10-29 21:08:05 -0400

    init commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require("http");
 const path = require("path");
 const WS_Controller = require("./app/web-scraping/src/webscraper_controller");
 const DataMigrator = require("./app/dataMigration/src/DataMigrator.js");
-const DbBuilder = require("./app/dataMigration/srs/DbBuilder.js");
+const DbBuilder = require("./app/dataMigration/src/DbBuilder.js");
 const bodyparser = require("body-parser");
 
 var app = express();
@@ -28,6 +28,7 @@ app.post("/buildDb", function(req, res) {
   dbBuilder.buildDb();
   res.send({ status: "Database Built!" });
 });
+
 app.post("/import", function(req, res) {
   console.log(req.body);
   let dataMigrator = new DataMigrator();
