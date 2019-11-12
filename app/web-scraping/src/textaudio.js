c6307b9b0b85f2932151bd6f6c2054354d62750f
Hadi Haidar <hadihaidar@Hadis-MacBook-Pro-2.local>
2019-11-11 20:01:48 -0500

    Test text audio

---
+++ app/web-scraping/src/textaudio.js
@@ -0,0 +1,7 @@
+/* ****************** TEXTAUDIO.JS ******************
+ * 2019 November 11 : Hadi Haidar : Created
+ ********************************************
+ * Purpose : File that runs saves files as text to audio *
+ */
+
+TextToSpeech.talk("Hello Beautiful World!");
